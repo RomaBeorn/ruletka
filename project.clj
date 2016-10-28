f62cf3c6b08ba506f8618b0c0407be4a10e00a26
Roma Matveev <matveev-logi@yandex.ru>
2016-10-28 22:10:54 +0000

    Update

--- project.clj
+++ project.clj
@@ -6,11 +6,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                 [ring "1.5.0"]
                 [ring/ring-defaults "0.1.5"]
-                [compojure "1.5.1"]
                 [org.clojure/java.jdbc "0.4.2"]
                 [mysql/mysql-connector-java "5.1.6"]
-                [clj-jade "0.1.7"]
-                [hiccup "1.0.5"]]
+                [clj-jade "0.1.7"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {
         :handler custom.core/app
--- src/custom/core.clj
+++ src/custom/core.clj
@@ -19,8 +19,11 @@
 (import 'java.security.MessageDigest)
 (import 'java.math.BigInteger)
 
+(def abc {:nam "1"})
+
+
 (defn parse-int [s]
-   (Integer. (re-find  #"\d+" s )))
+  (Integer. (re-find  #"\d+" s )))
 
 (defn insert-report [a b c]
     "signup user into database"
@@ -32,31 +35,6 @@
     
 (defn get-bugs []
   (sql/query db-config ["SELECT * FROM report"]))  
-
-(defn check-session [req] 
-    (println (str signup_login)))
-    (= (str signup_login) ""))
-
-(defn wrap-checks
-  "Checking MySQL Connection & Installed Flag Middleware"
-  [handler]
-  (fn [req]
-      (let [mysql-req (assoc req :uri "/mysql-enable")]
-                (println (req :uri))
-                (if (check-conn) 
-                    (if (check-session [req])
-                        (if (or 
-                                (= (str (req :uri)) "/signin")
-                                (= (str (req :uri)) "/signup")
-                                (= (str (req :uri)) "/home")
-                                (= (str (req :uri)) "/profile")
-                                )
-                            (handler req)
-                            (redirect "/home"))
-                    (redirect "/profile"))
-                    (handler mysql-req)))))
-                
-
   
   (defn md5 [s]
   (let [algorithm (MessageDigest/getInstance "MD5")
@@ -79,13 +57,14 @@
     (sql/insert! db-config :users {:user_name a :md5 (md5 (str b)) :user_role "999"}))
                 
 
-(defroutes athene-routes
-; Код, отвечающий за регистрацию.
+(defn check-session [req] 
+    (println (:name (:session req)))
+    (= (str (:name (:session req)))))
 
-        ; <-- -->
+(defroutes athene-routes
     (GET "/signup" [req]
              (layout/signup req))
-
+         
     (POST "/signup" [signup_login signup_password :as req]
           (println signup_login signup_password)
             (let [usernamesign {:user_name (str signup_login)}] 
@@ -93,9 +72,6 @@
                 "Пользователь с таким никнеймом уже существует"
                 (and (insert-user (str signup_login) signup_password) (redirect "/signin")))))
             
-    ;      <-- -->
-    ; Код, отвечающий за авторизацию.
-    ;      <-- -->
     (GET "/signin" [req]
             (layout/signin req))
         
@@ -104,45 +80,103 @@
             (let [username {:user_name (str signin_login)} signin_password_with_md5 {:md5 (str (md5 (str signin_password)))}]       
                 (if (= (str (list (check-username (str signin_login)))) (str "((" username "))"))
                     (if (= (str (list (check-password (str signin_login)))) (str "((" signin_password_with_md5 "))")) 
-                        (assoc (redirect "/profile") :session {:name signin_login :password signin_password}) 
+                        (assoc req :session {:name signin_login :password signin_password})
                         "Неправильный пароль")
-                        "Неправильный логин или пароль"))
-            )
-            
+                        "Неправильный логин или пароль")))
+                    
     (GET "/profile" [:as req]
+            (println (str (:password (:session req))))
             (html [:h1 (str "Имя: " (:name (:session req)))]
                   [:h1 (str "Это ваш пароль для доступа к учетной записи, запишите его на листок, чтобы не потерять - ") (str (:password (:session req)))]))
-    ;      <-- -->
-          
-           
-    (GET "/" []
+    
+    (GET "/" [:as req]
         (redirect "/home"))
-    (GET "/home" []
+    
+    (GET "/home" [:as req]
         (layout/home))
-    (GET "/mysql-enable" [] 
+    
+    (GET "/mysql-enable" [:as req] 
         (layout/db-not-conn))
-    (GET "/report" []
+    
+    (GET "/report" [req]
         (layout/report))
+    
     (POST "/report" [name text email :as req]
           (insert-report name text email)
           (redirect "/"))
+    
     (GET "/list-of-reports" []
          (layout/list-of-reports (get-bugs)))
+    
     (GET "/report/:id" [id]
          (delete-report id)
          (redirect "/list-of-reports"))
+    
     (GET "/install" []
          (install))
+    
     (GET "/contacts" []
          (layout/contacts))
+    
     ; (GET "/list-of-users" [req2]
     ;      (layout/users))
+    
     (route/resources "/"))
 
-(def app (->(routes athene-routes)
-            (wrap-defaults
-                (assoc-in site-defaults [:security :anti-forgery] false))
-            (wrap-params)
-            (wrap-session {:store (memory-store) :cookie-attrs {:max-age 360000}})
-            wrap-checks))
+(defn check-session [req]
+    (if (= (str (:name (:session req))) "")
+        (redirect "/home")))
+
+
+
+(defn wrap-checks
+  "Checking MySQL Connection & Installed Flag Middleware"
+  [handler]
+  (fn [req]
+      (let [mysql-req (assoc req :uri "/mysql-enable")]
+                (println (req :uri))
+                (println (:name (:session req)))
+                (if (check-conn) 
+                    (if (check-session req)
+                        (if (or 
+                                (= (str (req :uri)) "/signin")
+                                (= (str (req :uri)) "/signup")
+                                (= (str (req :uri)) "/home")
+                                (= (str (req :uri)) "/profile")
+                                )
+                            (handler req)
+                            (redirect "/home"))
+                        (handler req))
+                    (handler mysql-req)))))
+
+(def app (wrap-checks (wrap-session (wrap-params athene-routes) {:store (memory-store)} )))
           
+
+
+
+
+
+
+
+
+
+
+; (defn wrap-checks
+;   "Checking MySQL Connection & Installed Flag Middleware"
+;   [handler]
+;   (fn [req]
+;       (let [mysql-req (assoc req :uri "/mysql-enable")]
+;                 (println (req :uri))
+;                 (if (check-conn) 
+;                     (handler req)
+;                     (handler mysql-req)))))
+
+
+
+
+
+; (def app (->(routes athene-routes)
+;             (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
+;             (wrap-params)
+;             (wrap-session {:store (memory-store) :cookie-attrs {:max-age 360000}})
+;             (wrap-checks)))
